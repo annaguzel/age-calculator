7a189d157ef8a39cf4fd80b23efd19a2203edfce
Annaguzel <you@example.com>
2020-02-05 10:53:17 +0200

    updated age calculator

--- age_calculator.py
+++ age_calculator.py
@@ -1,12 +1,32 @@
 from datetime import datetime
+from datetime import time
+import datetime
 
 def check_birthdate(year, month, day):
-	# write code here
+	birthdate=datetime.date(year, month, day)
+	if (birthdate>=datetime.date.today()):
+		return False
+	else:
+		return True
+
 
 def calculate_age(year, month, day):
-    # write code here
+	birthdate=datetime.date(year, month, day)
+	today_year = datetime.date.today()
+	age=(today_year-birthdate)
+	age_in_years=age.days/365
+	print ("You are {} years old".format(age_in_years))
+
 
 def main():
+	year=int(input("Enter year of birth:"))
+	month=int(input("Enter month of birth:"))
+	day=int(input("Enter day of birth:"))
+
+	if (check_birthdate(year,month,day)==True):
+		calculate_age(year,month,day)
+	else:
+		print("Birthdate is invalid")
 	# write main code here
 
 
